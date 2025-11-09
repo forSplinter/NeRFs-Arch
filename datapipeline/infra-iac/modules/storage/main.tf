16fc0b18203a17ce39d0c81991518bf07fb58da8
forSplinter* <mugangamathys24@gmail.com>
2025-11-09 13:40:51 +0800

    Enhance S3 bucket security by enabling public access block and updating bucket policy

--- datapipeline/infra-iac/modules/storage/main.tf
+++ datapipeline/infra-iac/modules/storage/main.tf
@@ -7,6 +7,10 @@ resource "aws_s3_bucket" "nerfarc_s3_bucket_default" {
   
 }
 
+resource "aws_caller_identity" "name" {
+  
+}
+
 resource "aws_s3_object" "nerfarc_s3_bucket_upload" {
     for_each = toset([
         "raw/uploads/",
@@ -31,9 +35,33 @@ resource "aws_s3_object" "nerfarc_s3_bucket_upload" {
 resource "aws_s3_bucket_public_access_block" "diseable_public_access" {
     bucket = aws_s3_bucket.nerfarc_s3_bucket_default.id
 
-    block_public_acls = false
-    block_public_policy = false 
-    ignore_public_acls = false
-    restrict_public_buckets = false
+    block_public_acls = true
+    block_public_policy = true 
+    ignore_public_acls = true
+    restrict_public_buckets = true 
   
+}
+
+resource "aws_s3_bucket_policy" "nerfarc_s3_bucket_policy" {
+  bucket = aws_s3_bucket.nerfarc_s3_bucket_default.id
+  policy =  jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" //TODO: Refacto the arn role need to be the lambda here
+        Action = [
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.nerfarc_s3_bucket_default.arn,
+          "${aws_s3_bucket.nerfarc_s3_bucket_default.arn}*//*"
+        ]
+      }
+    ]
+  }) 
+  depends_on = [aws_s3_bucket_public_access_block.diseable_public_access]
 }
